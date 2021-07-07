537acd4b5787db5f154088869ad07fe300ceda34
paneedovanie <edovanie@ssagroup.com>
2021-07-07 17:20:15 +0800

    Modify header

--- src/components/headers/AppHeader.vue
+++ src/components/headers/AppHeader.vue
@@ -4,7 +4,7 @@
       <img alt="Vue logo" src="@/assets/logo.png">
     </div>
     <div class="app-header__nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
   </div>
